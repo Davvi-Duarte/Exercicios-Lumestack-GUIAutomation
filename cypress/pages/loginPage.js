49354b5dad62378a0015caafcb0fe95fc461cfb6
Davvi Duarte Rodrigues <49027886+Davvi-Duarte@users.noreply.github.com>
2025-05-20 23:18:29 -0300

    Update loginPage.js
    Refatoração dos testes

--- cypress/pages/loginPage.js
+++ cypress/pages/loginPage.js
@@ -4,10 +4,16 @@ class LoginPage {
             usernameField: "[name='username']",
             passwordField: "[name='password']",
             loginButton:"[data-test='signin-submit']",
-            loginErrorMessage: "[data-test='signin-error']"
+            loginErrorMessage: "[data-test='signin-error']",
+            singupButton: "[data-test='signup']",
+            singInTile: "[.MuiTypography-h5]"
         };
         return selectors;
     };
+
+    checkLoginPageTitle(){
+        cy.get(this.selectorsList().singInTile).should('be.visible');
+    }
     
     accessLoginPage(){
         return cy.visit('/singin');
@@ -17,11 +23,15 @@ class LoginPage {
         cy.get(this.selectorsList().usernameField).type(username);
         cy.get(this.selectorsList().passwordField).type(password);
         cy.get(this.selectorsList().loginButton).click();
-    }
+    };
+
+    openSingUpPage(){
+        cy.get(this.selectorsList().singupButton).click();
+    };
 
     checkInvalidLoginMessage(){
         cy.get(this.selectorsList().loginErrorMessage).should('be.visible');
-    }
+    };
 };
 
 export default LoginPage;
